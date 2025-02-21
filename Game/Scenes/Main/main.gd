fe94a1a819d739a156d70b18792ec60aa0888981
Scott DePouw <scott.depouw@gmail.com>
2025-02-21 10:50:36 -0500

    Stub credits, and boilerplate for loading scenes

--- Game/Scenes/Main/main.gd
+++ Game/Scenes/Main/main.gd
@@ -7,16 +7,45 @@ extends Node
 # from "Squakross: Home Squeak Home" https://www.squeakross.cool/
 
 @onready var _gameScene: PackedScene = preload("res://Scenes/Game/game.tscn")
-var _gameInstance: Game
+@onready var _creditsScene: PackedScene = preload("res://Scenes/Credits/credits.tscn")
+var _loadedInstance: Node
+var _sceneToLoad: PackedScene
+var _loadQueued: bool:
+  get: return _sceneToLoad != null
 
 func _ready() -> void:
-  _play_game() # TODO: Menu item
+  _on_play_clicked() # Pretend we clicked "Play Game"
+  await get_tree().create_timer(3.0).timeout
+  _on_credits_clicked() # Pretend we accessed the Credits 3 seconds later.
 
-func _play_game() -> void:
-  _gameInstance = _gameScene.instantiate()
-  add_child(_gameInstance) # calls _ready(), etc. at this moment
+func _on_play_clicked() -> void:
+  _queue_load(_gameScene)
 
-func _exit_game() -> void:
-  if !is_instance_valid(_gameInstance):
+func _on_credits_clicked() -> void:
+  _queue_load(_creditsScene)
+
+func _queue_load(scene: PackedScene) -> void:
+  if _loadQueued:
     return
-  _gameInstance.queue_free()
+  _sceneToLoad = scene;
+
+func _load_instance(scene: PackedScene) -> void:
+  _loadedInstance = scene.instantiate()
+  add_child(_loadedInstance)
+
+func _unload_instance() -> void:
+  if _loadedInstance == null or !is_instance_valid(_loadedInstance):
+    return
+  _loadedInstance.queue_free()
+
+func _process(_delta: float) -> void:
+  if !_loadQueued:
+    return
+  if _loadedInstance != null:
+    if is_instance_valid(_loadedInstance):
+      _unload_instance()
+      return;
+    _loadedInstance = null
+  if _loadedInstance == null:
+    _load_instance(_sceneToLoad)
+    _sceneToLoad = null
