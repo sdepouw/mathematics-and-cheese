1aedc8babd9462e4707a09065816542f736be2f3
Scott DePouw <scott.depouw@gmail.com>
2025-02-21 14:01:05 -0500

    Project Game Name + Tweaks

--- Game/Scenes/MainMenu/main_menu.gd
+++ Game/Scenes/MainMenu/main_menu.gd
@@ -1,6 +1,11 @@
 class_name MainMenu
 extends Node
 
+@onready var _gameTitleLabel: Label = $GameTitleLabel
+
+func _ready() -> void:
+  _gameTitleLabel.text = ProjectSettings.get_setting("application/config/name")
+
 func _on_play_button_pressed() -> void:
   EventBus.load_game.emit()
 
