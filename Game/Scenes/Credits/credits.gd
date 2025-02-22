f4f7b45cc79963be3f03926712a56432310807f6
Scott DePouw <scott.depouw@gmail.com>
2025-02-21 21:06:19 -0500

    Refactor

--- Game/Scenes/Credits/credits.gd
+++ Game/Scenes/Credits/credits.gd
@@ -1,13 +1,11 @@
 class_name CreditsScreen
 extends Node
 
+@onready var _creditsRichTextLabel: RichTextLabel = $CreditsText
+
 func _on_back_button_pressed() -> void:
   EventBus.load_main_menu.emit()
 
 func _ready() -> void:
   if OS.get_name() != "HTML5":
-    var label: RichTextLabel = $CreditsText
-    label.meta_clicked.connect(_on_RichTextLabel_meta_clicked)
-
-func _on_RichTextLabel_meta_clicked(meta: String) -> void:
-  OS.shell_open(meta)
+    _creditsRichTextLabel.meta_clicked.connect(OS.shell_open)
