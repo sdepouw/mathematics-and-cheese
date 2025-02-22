31774390a5aca46ed3695abc0bd12fba36878c41
Scott DePouw <scott.depouw@gmail.com>
2025-02-22 08:51:57 -0500

    Extracted RichTextLabel behavior into custom Scene
    Making all BBCode URLs clickable by default

--- Game/Scenes/Credits/credits.gd
+++ Game/Scenes/Credits/credits.gd
@@ -1,11 +1,5 @@
 class_name CreditsScreen
 extends Node
 
-@onready var _creditsRichTextLabel: RichTextLabel = $CreditsText
-
 func _on_back_button_pressed() -> void:
   EventBus.load_main_menu.emit()
-
-func _ready() -> void:
-  if OS.get_name() != "HTML5":
-    _creditsRichTextLabel.meta_clicked.connect(OS.shell_open)
