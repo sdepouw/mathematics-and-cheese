faa8aeab4c816b2de457c608f3df299d152611ee
Scott DePouw <scott.depouw@gmail.com>
2025-02-22 14:29:38 -0500

    Make preload() calls constants

--- Game/Scenes/Root/root.gd
+++ Game/Scenes/Root/root.gd
@@ -5,23 +5,24 @@ extends Node
 # TODO: Update Reticle with custom sprite
 # TODO: Sound effects
 
+const SPLASH_SCREEN_SCENE: PackedScene = preload("res://Scenes/SplashScreen/splash_screen.tscn")
+const MAIN_MENU_SCENE: PackedScene = preload("res://Scenes/MainMenu/main_menu.tscn")
+const GAME_SCENE: PackedScene = preload("res://Scenes/Game/game.tscn")
+const CREDITS_SCENE: PackedScene = preload("res://Scenes/Credits/credits.tscn")
+
 @onready var _sceneLoader: SceneLoader = $SceneLoader
-@onready var _splashScreenScene: PackedScene = preload("res://Scenes/SplashScreen/splash_screen.tscn")
-@onready var _mainMenuScene: PackedScene = preload("res://Scenes/MainMenu/main_menu.tscn")
-@onready var _gameScene: PackedScene = preload("res://Scenes/Game/game.tscn")
-@onready var _creditsScene: PackedScene = preload("res://Scenes/Credits/credits.tscn")
 
 func _ready() -> void:
-  _sceneLoader.queue_load(_splashScreenScene)
+  _sceneLoader.queue_load(SPLASH_SCREEN_SCENE)
   EventBus.load_main_menu.connect(_load_main_menu)
   EventBus.load_game.connect(_load_game)
   EventBus.load_credits.connect(_load_credits)
 
 func _load_main_menu() -> void:
-  _sceneLoader.queue_load(_mainMenuScene)
+  _sceneLoader.queue_load(MAIN_MENU_SCENE)
 
 func _load_game() -> void:
-  _sceneLoader.queue_load(_gameScene)
+  _sceneLoader.queue_load(GAME_SCENE)
 
 func _load_credits() -> void:
-  _sceneLoader.queue_load(_creditsScene)
+  _sceneLoader.queue_load(CREDITS_SCENE)
