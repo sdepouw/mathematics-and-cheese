758da14d2066910f6de13340afab433ce2f79cef
Scott DePouw <scott.depouw@gmail.com>
2025-02-24 19:04:37 -0500

    Remove duplicated stuff

--- Game/Scenes/EquationBoard/euqation_board.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# 6 Markers for now (9 later)
-# Instantiate 6 Equations
-const EQUATION_SCENE: PackedScene = preload("res://Scenes/Equation/equation.tscn")
-
-signal equation_selected(answer: int)
-
-func _ready() -> void:
