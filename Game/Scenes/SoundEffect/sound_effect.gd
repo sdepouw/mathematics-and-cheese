f8e1a78adea56e29646544312c75edde45e63ef4
Scott DePouw <scott.depouw@gmail.com>
2025-02-26 17:59:22 -0500

    Comment and clamp

--- Game/Scenes/SoundEffect/sound_effect.gd
+++ Game/Scenes/SoundEffect/sound_effect.gd
@@ -1,7 +1,10 @@
+## An AudioStreamPlayer whose volume is adjusted by a global Sound Effect Volume
+## option.
+class_name SoundEffect
 extends AudioStreamPlayer
 
 # TODO: Expose via Options menu
 const sound_volume: float = 1.0
 
 func _ready() -> void:
-  self.volume_linear *= sound_volume
+  self.volume_linear *= clampf(sound_volume, 0.0, 1.0)
