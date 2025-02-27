36ded0030d3b0cedafbd62e519100edbc2aac6b1
Scott DePouw <scott.depouw@gmail.com>
2025-02-27 08:00:47 -0500

    TODO and variable rename

--- Game/Scenes/OptionsScreen/options_screen.gd
+++ Game/Scenes/OptionsScreen/options_screen.gd
@@ -2,7 +2,7 @@ class_name OptionsScreen
 extends Control
 
 @onready var _clear_confirm_modal: ConfirmationModal = $ClearConfirmModal
-@onready var _sound_amount: ProgressBar = $SoundAmount
+@onready var _sound_volume_progress_bar: ProgressBar = $SoundAmount
 
 # TODO: Apply saved sound option to our Sound Effect button
 
@@ -18,15 +18,16 @@ func _ready() -> void:
 func _on_sound_amount_gui_input(event: InputEvent) -> void:
   if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
     var mouse_event: InputEventMouse = event as InputEventMouse
-    var clicked_spot: float = clampf(mouse_event.position.x, 0, _sound_amount.size.x)
-    _sound_amount.mouse_default_cursor_shape = Control.CURSOR_HSPLIT
-    _options.sound_volume = clicked_spot / _sound_amount.size.x
+    var clicked_spot: float = clampf(mouse_event.position.x, 0, _sound_volume_progress_bar.size.x)
+    _sound_volume_progress_bar.mouse_default_cursor_shape = Control.CURSOR_HSPLIT
+    _options.sound_volume = clicked_spot / _sound_volume_progress_bar.size.x
     _set_sound_amount_value(_options.sound_volume)
   else:
-    _sound_amount.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+    _sound_volume_progress_bar.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
 
+# TODO: This works when bar's value is 0-1, but not when the min/max are other values.
 func _set_sound_amount_value(value: float) -> void:
-  _sound_amount.value = (_sound_amount.min_value + _sound_amount.max_value) * value
+  _sound_volume_progress_bar.value = (_sound_volume_progress_bar.min_value + _sound_volume_progress_bar.max_value) * value
 
 func _on_clear_data_button_pressed() -> void:
   var confirmed: bool = await _clear_confirm_modal.prompt()
