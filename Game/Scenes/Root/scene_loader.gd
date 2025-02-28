6ab0a54ba6c366487f9569af95b0dd8e1ecc9371
Scott DePouw <scott.depouw@gmail.com>
2025-02-28 07:57:41 -0500

    Scene Loader can now pass data
    Checks if scene has "func load_scene_data(data: Variant)" as a method to call, before calling. Data is passed as Variant.

--- Game/Scenes/Root/scene_loader.gd
+++ Game/Scenes/Root/scene_loader.gd
@@ -6,6 +6,7 @@ extends Node
 
 var _loaded_instance: Node
 var _scene_to_load: PackedScene
+var _data_for_scene_to_load: Variant
 var _load_queued: bool:
   get: return _scene_to_load != null
 
@@ -16,10 +17,11 @@ signal instance_loaded(loaded_scene: PackedScene, loaded_instance: Node)
 ## be unloaded properly before this scene will load.
 ## If another PackedScene was queued for loading but never got loaded,
 ## this new call will take priority.
-func queue_load(scene: PackedScene) -> void:
+func queue_load(scene: PackedScene, data_for_scene: Variant = null) -> void:
   # HACK: Allow any sound effects to play
   await get_tree().create_timer(.15).timeout
   _scene_to_load = scene;
+  _data_for_scene_to_load = data_for_scene
 
 func _process(_delta: float) -> void:
   if !_load_queued:
@@ -32,9 +34,28 @@ func _process(_delta: float) -> void:
 
 func _load_instance() -> void:
   _loaded_instance = _scene_to_load.instantiate()
+  # If we're given data to pass along, and the scene has func "load_scene_data(data: Variant)", call it
+  if _data_for_scene_to_load != null && _instance_has_load_scene_data_method(_loaded_instance):
+    @warning_ignore("unsafe_method_access")
+    _loaded_instance.load_scene_data(_data_for_scene_to_load)
   get_parent().add_child(_loaded_instance)
   instance_loaded.emit(_scene_to_load, _loaded_instance)
 
 func _try_unload_instance() -> void:
   if _loaded_instance != null and is_instance_valid(_loaded_instance):
     _loaded_instance.queue_free()
+
+func _instance_has_load_scene_data_method(instance: Node) -> bool:
+  var methods: Array[Dictionary] = instance.get_method_list()
+  var load_method_metadata: Dictionary
+  for method: Dictionary in methods:
+    if method["name"] == "load_scene_data":
+      load_method_metadata = method
+      break
+  if not load_method_metadata:
+    return false
+  var args: Array[Dictionary] = load_method_metadata["args"]
+  if args.size() != 1:
+    return false
+  var argument: Dictionary = args[0]
+  return argument["name"] == "data" and argument["type"] == TYPE_NIL
