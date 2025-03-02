f4f5cdf17ce871d573275809e954e75ec51e35e9
Scott DePouw <scott.depouw@gmail.com>
2025-03-02 08:46:05 -0500

    Handle tallying in _ready()
    Orchestrate everything in _ready() instead of relying on _process() with lots of boolean flags

--- Game/Scenes/GameOverScreen/game_over_screen.gd
+++ Game/Scenes/GameOverScreen/game_over_screen.gd
@@ -14,6 +14,9 @@ extends Control
 @onready var _sum_line: Line2D = %SumLine
 @onready var _total_line: HBoxContainer = %TotalHBox
 
+@onready var _score_line_timer: Timer = $ScoreLineTimer
+@onready var _tally_timer: Timer = $StreakTallyTimer
+
 var _score: int
 var _streak_score: int
 var _total_score: int
@@ -28,20 +31,22 @@ var _current_total_score: int:
     _current_total_score = value
     _total_value.text = "%d" % value
 
-var _tallying: bool
-var _waited_for_total: bool
-var _done_tallying: bool
-
 func load_scene_data(data: Variant) -> void:
   # data is Array[int]
   _score = data[0]
   _best_streak = data[1]
 
 func _ready() -> void:
-  # Debug
-  _score = 100000
-  _best_streak = 20
+  load_scene_data([100000, 20]) # Debug
+  _streak_score = _best_streak * best_streak_bonus_unit
+  _total_score = _score + _streak_score
+  _initialize_tally_display()
+  await _tally_async()
+  print("Done!")
+  # TODO: Show 'high score congrats' or 'thanks for playing' message
+  # TODO: Show 'play again' and 'back to menu' buttons
 
+func _initialize_tally_display() -> void:
   _score_line.hide()
   _streak_line.hide()
   _sum_line.hide()
@@ -50,37 +55,25 @@ func _ready() -> void:
   _streak_value.text = "%03d" % _best_streak
   _streak_score_value.text = "0"
   _total_value.text = "0"
-  _streak_score = _best_streak * best_streak_bonus_unit
-  _total_score = _score + _streak_score
-  await get_tree().create_timer(.5).timeout
+
+func _tally_async() -> void:
+  _score_line_timer.start()
+  await _score_line_timer.timeout
   _score_line.show()
-  await get_tree().create_timer(.5).timeout
-  _tallying = true
-
-func _process(_delta: float) -> void:
-  if not _done_tallying:
-    _try_tally()
-  else:
-    pass
-    # Show high score congrats, or general 'game over' message
-    # Show Play Again / Back to Menu buttons
-
-func _try_tally() -> void:
-  if not _tallying:
-    return
-  if _current_streak_score < _streak_score:
-    if not _streak_line.visible:
-      _streak_line.show()
-    _current_streak_score = (clamp(_current_streak_score + round(best_streak_bonus_unit / 2), 0, _streak_score))
-    return
-  if not _waited_for_total:
-    await get_tree().create_timer(.5).timeout
-    _waited_for_total = true
-  if _current_total_score < _total_score:
-    if not _total_line.visible:
-      _sum_line.show()
-      _total_line.show()
-    _current_total_score = clampi(_current_total_score + 1039, 0, _total_score)
-  else:
-    _tallying = false
-    _done_tallying = true
+  _score_line_timer.start()
+  await _score_line_timer.timeout
+  _streak_line.show()
+  while _current_streak_score < _streak_score:
+    var increment: int = _current_streak_score + best_streak_bonus_unit
+    _tally_timer.start()
+    await _tally_timer.timeout
+    _current_streak_score = clampi(increment, 0, _streak_score)
+  _score_line_timer.start()
+  await _score_line_timer.timeout
+  _sum_line.show()
+  _total_line.show()
+  while _current_total_score < _total_score:
+    var increment: int = _current_total_score + 5276
+    _tally_timer.start()
+    await _tally_timer.timeout
+    _current_total_score = clampi(increment, 0, _total_score)
